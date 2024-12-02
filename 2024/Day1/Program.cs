481a4146a7cd49ce4157101aa24427d2c61ba4e4
Adam Lear <aalear@users.noreply.github.com>
2024-12-01 23:42:55 -0500

    replace foreach with Aggregate

--- 2024/Day1/Program.cs
+++ 2024/Day1/Program.cs
@@ -1,17 +1,13 @@
-﻿var input = File.ReadAllLines("input.txt");
+﻿var lists = File.ReadAllLines("input.txt")
+    .Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray())
+    .Aggregate((Left: new List<int>(), Right: new List<int>()), (acc, vals) =>
+    {
+        acc.Left.Add(vals[0]);
+        acc.Right.Add(vals[1]);
+        return acc;
+    });
 
-var left = new List<int>();
-var right = new List<int>();
-
-foreach (var line in input)
-{
-    var numbers = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-    left.Add(numbers[0]);
-    right.Add(numbers[1]);
-}
-
-left.Sort();
-right.Sort();
-
-Console.WriteLine($"Part 1: {left.Zip(right, (l, r) => Math.Abs(r - l)).Sum()}");
-Console.WriteLine($"Part 2: {left.Sum(num => num * right.Count(n => n == num))}");
+Console.WriteLine($@"Part 1: {lists.Left.OrderBy(i => i)
+                                        .Zip(lists.Right.OrderBy(i => i), (l, r) => Math.Abs(r - l))
+                                        .Sum()}");
+Console.WriteLine($"Part 2: {lists.Left.Sum(num => num * lists.Right.Count(n => n == num))}");
