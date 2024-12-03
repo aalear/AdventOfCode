6f6f88c87d870d2b0aa7596685150a4d4dee8f81
Adam Lear <aalear@users.noreply.github.com>
2024-12-03 14:38:57 -0500

    day 3

---
+++ 2024/c#/Day3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+var input = string.Join("", File.ReadAllLines("Day3.txt"));
+var mulRegex = new Regex(@"mul\((?<first>\d{1,3}),(?<second>\d{1,3})\)");
+
+var part1 = mulRegex.Matches(input).Sum(MulValue);
+
+var onlyEnabled = Regex.Replace(input, @"(don't\(\)).*?(do\(\)|$)", string.Empty);
+var part2 = mulRegex.Matches(onlyEnabled).Sum(MulValue);
+
+Console.WriteLine($"Part 1: {part1}");
+Console.WriteLine($"Part 2: {part2}");
+return;
+
+int MulValue(Match match) => int.Parse(match.Groups["first"].Value) * int.Parse(match.Groups["second"].Value);
